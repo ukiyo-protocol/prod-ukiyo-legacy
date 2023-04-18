b82eed765d7755260ac4f69da2c0514c5c9799b6
BowTiedSwan <0xf85ce443D3F031728e0e97503C06322c2F51A0c7@ethmail.cc>
2023-04-18 15:30:48 +0300

    add post types and render

--- src/components/Pages/LandingPage/Components/TheNews/TheNews.tsx
+++ src/components/Pages/LandingPage/Components/TheNews/TheNews.tsx
@@ -1,39 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Button } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./TheNews.scss";
 import { Link } from "react-scroll";
 import rightArrow from "../../../../../assets/Images/arrow.svg";
+import sanityClient from "../../../../../sanityClient";
+
+interface Post {
+  title: string;
+  slug: { current: string };
+  body: any;
+  publishedAt: string;
+  mainImage: {
+    asset: {
+      _id: string;
+      url: string;
+    };
+  };
+}
 
 function TheNews() {
-  const data = [
-    {
-      id: 1,
-      headings: "ukiyo Q4 2022 Report",
-      rightArrow: rightArrow,
-      Date: "Aug 04, 2022",
-      Defination:
-        "In September 2021, ukiyo was conceptualized. Development swiftly began, and one year later we release our first report to help the community better understand on how the DAO is getting along.",
-    },
-    {
-      id: 3,
-      headings: "Phase I Seed Round – Private Sale",
-      rightArrow: rightArrow,
-      Date: "Aug 04, 2022",
-      Defination:
-        " As we prepare for the launch of the private sale, find out how you can participate and help build a world defined by borderless capital that will act as a catalyst for wealth creation.",
-    },
-    {
-      id: 2,
-      headings: "Fundamentals: Encouraging Venture Investment On DeFi",
-      rightArrow: rightArrow,
-      Date: "Aug 04, 2022",
-      Defination:
-        "The brainchild and founding vision of the ukiyo Protocol. Learn what it aims to achieve and how its roadmap will forever change the landscape of venture capital.",
-    },
 
-  ];
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"] | order(publishedAt desc) [0...2] {
+          title,
+          slug,
+          body,
+          publishedAt,
+          mainImage {
+            asset -> {
+              _id,
+              url
+            }
+          }
+        }`
+      )
+      .then((data) => setPosts(data))
+      .catch(console.error);
+  }, []);
 
   return (
     <div id="news_sec" className="The-NewsSec product_update">
@@ -44,26 +53,27 @@ function TheNews() {
             <button className="btn-style btnGradient" data-aos="fade-up">View All Articles</button>
           </Col>
           <Col lg={9} data-aos="fade-up">
-            {data.map((user, key) => (
-              <div className="news-post" key={`index_${key}`}>
-                {/* <Link to=""> */}
-                <div className="heading" data-aos="fade-up">
-                  <h3>{user.headings}</h3>
-                  <Button className="news_button">
-                    <img src={user.rightArrow} alt="img" />{" "}
-                  </Button>
-                </div>
-                <Row className="news_row" data-aos="fade-up">
-                  <Col lg={2} data-aos="fade-up">
-                    <p>{user.Date}</p>
-                  </Col>
-                  <Col lg={6} data-aos="fade-up">
-                    <p>{user.Defination}</p>
-                  </Col>
-                </Row>
-                {/* </Link> */}
-              </div>
-            ))}
+            {posts &&
+              posts.map((post, index) => (
+                <Link to={"/news/" + post.slug.current} key={post.slug.current}>
+                  <div className="news-post" key={index}>
+                    <div className="heading" data-aos="fade-up">
+                      <h3>{post.title}</h3>
+                      <Button className="news_button">
+                        <img src={rightArrow} alt="img" />{" "}
+                      </Button>
+                    </div>
+                    <Row className="news_row" data-aos="fade-up">
+                      <Col lg={2} data-aos="fade-up">
+                        <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+                      </Col>
+                      <Col lg={6} data-aos="fade-up">
+                        {/* <p>{postBrief(post.body)}...</p> */}
+                      </Col>
+                    </Row>
+                  </div>
+                </Link>
+              ))}
             <button className="btn-style btnGradient new_one" data-aos="fade-up">View all articles</button>
           </Col>
         </Row>
